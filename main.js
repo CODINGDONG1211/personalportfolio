689326a539c251f3270166304a3f514b075b72ae
Praveen Ramspra <you@example.com>
2024-04-20 01:29:25 +0530

    font and color changes

--- main.js
+++ main.js
@@ -25,3 +25,55 @@ inputs.forEach(ipt =>{
     ipt.parentNode.classList.remove("focus");
   });
 });
+
+// var frontend=document.querySelector(".frontend");
+// var backend=document.querySelector(".backend");
+// var dsa=document.querySelector(".dsa");
+// backend.classList.add("hidden");
+// dsa.classList.add("hidden");
+
+
+// var skillbuttons=document.querySelectorAll(".skill-btn");
+
+// skillbuttons.forEach(item=>{
+//   item.addEventListener("click",()=>{
+//     console.log("this");
+//     // var frontend=document.querySelector(".frontend");
+//     // var backend=document.querySelector(".backend");
+//     // var dsa=document.querySelector(".dsa");
+//     if(item.innerHTML==="frontend"){
+//       frontend.setAttribute("hidden","hidden");
+//       backend.classList.add("hidden");
+//       dsa.classList.add("hidden");
+//     }
+//     else if (item.innerHTML==="backend") {
+//       frontend.classList.add("hidden");
+//       backend.classList.remove("hidden");
+//       dsa.classList.add("hidden");
+//     } else {
+//       frontend.classList.add("hidden");
+//       backend.classList.add("hidden");
+//       dsa.classList.remove("hidden");
+//     }
+//   })
+
+// });
+
+document.getElementById("defaultOpen").click();
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
